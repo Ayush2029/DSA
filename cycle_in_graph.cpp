56d5ac1deef24e734cd70a0abafde6ba13cbe029
Ayush2029 <139906238+Ayush2029@users.noreply.github.com>
2023-12-30 17:09:29 +0530

    Check whether their is a cycle or not in a graph

---
+++ cycle_in_graph.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "bits/stdc++.h"
+#define pb push_back
+#define FOR(i, a, b) for (ll i = (a); i < (b); ++i)
+#define FORi(i, a, b) for (ll i = (a); i <=(b); ++i)
+#define REV(i, a, b) for (ll i = (a); i > (b); --i)
+#define REVi(i, a, b) for (ll i = (a); i >= (b); --i)
+#define s(a) sort(a.begin(),a.end())
+#define all(x) (x).begin(), (x).end()
+#define mod 1000000007
+using namespace std;
+using ll = long long;
+using ld = long double;
+using str = string;
+using vi = vector<int>;
+using vl = vector<long long>;
+using vs = vector<string>;
+using mi = map<int,int>;
+using ml = map<ll,ll>;
+using mu = unordered_map<int,int>;
+using mlu = unordered_map<ll,ll>;
+using vvl = vector<vector<ll>>;
+/*
+ * author: ayushd2910
+ */
+bool DFS(ll nodes, vector<bool>&visited, vector<vector<ll>>&arr, ll root,ll par){
+    visited[root]=true;
+    bool ans= false;
+    FOR(i,0,arr[root].size()){
+        if(visited[arr[root][i]]) {
+            if(arr[root][i]!=par)
+                return true;
+        }
+        else ans|=DFS(nodes,visited,arr,arr[root][i],root);
+    }
+    return ans;
+}
+void solve() {
+    ll nodes,vertices;
+    cin>>nodes>>vertices;
+    vvl arr(nodes+1);
+    FOR(i,0,vertices){
+        ll x,y;
+        cin>>x>>y;
+        arr[x].pb(y);
+        arr[y].pb(x);
+    }
+    vector<bool>visited(nodes+1,false);
+    cout<<DFS(nodes,visited,arr,1,0);
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll z=1;
+    cin>>z;
+    while(z--){
+        solve();
+    }
+    return 0;
+}
